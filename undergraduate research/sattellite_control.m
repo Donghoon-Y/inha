a184e13fa5089eee52db8fddaaebca5cc1f9e63e
hoony <hoony@HoonyMac.local>
2025-12-27 19:39:41 +0900

    undergraduate

---
+++ undergraduate research/sattellite_control.m
@@ -0,0 +1,119 @@
+clc; clear all;
+
+%Parameter
+mu = 398600;        % [km3/s2] Earth gravitational constant
+R = 6371;           % [km] Earth radius
+a = 600+R;          % [km] semi-major axis (SMA)(circle orbit)
+e = 0;              % [-] eccentricity
+inc = 97.8;         % [deg] inclination
+raan = 0;           % [deg] 승교점이각
+aop = 0;            % [deg] 근점이각
+lat = 37.2302;      % [deg] 위도(항공우주산학융합원)
+lon = 126.3925;     % [deg] 경도
+alt = 0;            % [m] 해발고도
+angle = 10;         % [deg] 최소교신각도
+
+% Earth sphere
+[xe, ye, ze] = sphere(80);  
+Re = R;                      
+
+xe = Re * xe;
+ye = Re * ye;
+ze = Re * ze;
+
+%position 
+r_pqw = [a; 0; 0];                    % km
+v_pqw = [0; sqrt(mu/a); 0];           % km/s
+
+%PQW to ECI rotation
+R_W = [cos(deg2rad(raan)) -sin(deg2rad(raan)) 0;
+       sin(deg2rad(raan)) cos(deg2rad(raan)) 0;
+       0 0 1];
+R_i = [1 0 0;
+       0 cos(deg2rad(inc)) -sin(deg2rad(inc));
+       0 sin(deg2rad(inc)) cos(deg2rad(inc))];
+R_w = [cos(deg2rad(aop)) -sin(deg2rad(aop)) 0;
+       sin(deg2rad(aop)) cos(deg2rad(aop)) 0;
+       0 0 1];
+pqw2eci = R_W*R_i*R_w;
+
+%Time 
+t_kst = datetime(2025,12,19,0,0,0,'TimeZone','Asia/Seoul');
+t_utc0 = t_kst;
+t_utc0.TimeZone = 'UTC';
+
+%Two Body Dynamics -> 원궤도가 되었을 때 타원파라미터를 쉽게 정의하기 위해서 타원식으로 전개
+r0 = pqw2eci*r_pqw;
+v0 = pqw2eci*v_pqw;
+p = a*(1-e^2); 
+b = a*sqrt(1-e^2);
+T = 2*pi*sqrt(a^3/mu);
+n = 2*pi/T;
+h = sqrt(mu*p);
+x0 = [r0; v0];
+tspan = [0 3*T];
+
+[t, x] = ode89(@(t, x) odeTwoBody(t, x, mu), tspan, x0);
+
+r_eci = x(:,1:3);
+v_eci = x(:,4:6);
+
+%ECI to ECEF 
+r_ecef = zeros(size(r_eci));
+v_ecef = zeros(size(v_eci));
+
+t_utc = t_utc0 + seconds(t);
+
+for i = 1:length(t)
+    utc_vec = datevec(t_utc(i));
+    mjd = mjuliandate(utc_vec);
+    pm = polarMotion(mjd);
+    [re_m, ve_m] = eci2ecef(utc_vec, r_eci(i, :)*1000, v_eci(i, :)*1000, 'pm', pm);
+    r_ecef(i,:) = re_m/1000;
+    v_ecef(i,:) = ve_m/1000;
+
+end
+
+%ECI, ECEF Plot
+figure; hold on;
+
+surf(xe, ye, ze, ...
+    'FaceColor',[0.6 0.8 1], ...
+    'EdgeColor','none', ...
+    'FaceAlpha',0.3, ...
+    'DisplayName','Earth');
+
+plot3(r_eci(:,1), r_eci(:,2), r_eci(:,3), ...
+    'LineWidth',1.5, ...
+    'DisplayName','ECI');
+
+plot3(r_ecef(:,1), r_ecef(:,2), r_ecef(:,3), ...
+    'LineWidth',1.5, ...
+    'DisplayName','ECEF');
+
+axis equal; grid on;
+xlabel('X [km]'); ylabel('Y [km]'); zlabel('Z [km]');
+title('Satellite trajectory');
+legend('Location','best');
+
+view(3);
+rotate3d on;
+
+
+%Sun postion 
+
+
+%function
+function xdot = odeTwoBody(t,x,mu)
+  
+    % input
+    r = x(1:3);
+    v = x(4:6);
+    rmag = norm(r);
+
+    % derivative of state vector
+    rdot = v;
+    vdot = - mu / rmag^3 * r;
+    xdot = [rdot ; vdot];
+    
+end
